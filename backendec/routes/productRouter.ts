956832680559062e165fddaa1cb98cb7deed91e5
allthetok <al.tokjuman@gmail.com>
2024-01-28 20:44:52 -0500

    product route working, format response object with productReq for product, similarProducts for other products with same model id

--- backendec/routes/productRouter.ts
+++ backendec/routes/productRouter.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 require('dotenv').config()
-import express, { NextFunction, Request, Response } from 'express'
+import express, { NextFunction, Request, Response, raw } from 'express'
 import SQL from 'sql-template-strings'
 import { pool } from '../src/db'
 import { Brands, IndProduct } from '../helpers/betypes'
@@ -35,7 +35,10 @@ router.post('/product', async (request: Request, response: Response) => {
 	const body = request.body
 	const productReq: string = body.product
 	let productQueryResult: any
+	let rawQueryResult: any
 	let productExists = true
+	let productRequested: any
+	let similarProducts: any[]
 
 	if (!productReq || productReq === null || productReq === undefined || productReq === '') {
 		return response.status(404).json({
@@ -58,61 +61,6 @@ router.post('/product', async (request: Request, response: Response) => {
 		})
 	}
 
-	// await pool.query(SQL`
-	// 	SELECT p.id, b.name AS brand, b.id AS brandId, m.id AS modelId, m.name AS modelName, p.name, p.releaseDate, p.colors, p.price, p.description, s.colSize AS sizes
-	// 		FROM products p
-	// 		INNER JOIN brands b ON p.brandId = b.id
-	// 		INNER JOIN models m ON p.modelId = m.id AND b.id = m.brandId
-	// 		INNER JOIN sizes s ON p.id = s.id
-	// 		WHERE p.name=${productReq}
-	// 	`)
-	// 	.then((response: any) => {
-	// 		console.log(response.rows)
-	// 		productQueryResult = response.rows[0]
-	// 	})
-	// 	.catch((err: any) => {
-	// 		console.log(err)
-	// 		return response.status(404).json({
-	// 			error: `Unable to retrieve product: ${productReq} from database`
-	// 		})
-	// 	})
-
-	// await pool.query(SQL`
-	// 	WITH prod_req AS (
-	// 		SELECT p.id, b.name AS brand, b.id AS brandId, m.id AS modelId, m.name AS modelName, p.name, p.releaseDate, p.colors, p.price, p.description, s.colSize AS sizes
-	// 		FROM products p
-	// 		INNER JOIN brands b ON p.brandId = b.id
-	// 		INNER JOIN models m ON p.modelId = m.id AND b.id = m.brandId
-	// 		INNER JOIN sizes s ON p.id = s.id
-	// 		WHERE p.name=${productReq}
-	// 	),
-	// 	WITH similar_prod AS (
-	// 		SELECT p.id, b.name AS brand, b.id AS brandId, m.id AS modelId, m.name AS modelName, p.name, p.releaseDate, p.colors, p.price, p.description, s.colSize AS sizes
-	// 		FROM products p
-	// 		INNER JOIN brands b ON p.brandId = b.id
-	// 		INNER JOIN models m ON p.modelId = m.id AND b.id = m.brandId
-	// 		INNER JOIN sizes s ON p.id = s.id
-	// 		WHERE p.modelId = prod_req.modelId AND p.name <> ${productReq}
-	// 	)
-	// 	SELECT pr.*, ps.* FROM prod_req pr, similar_prod ps;`)
-	// await pool.query(SQL`
-	// 	WITH prod_req AS (
-	// 		SELECT p.id, b.name AS brand, b.id AS brandId, m.id AS modelId, m.name AS modelName, p.name, p.releaseDate, p.colors, p.price, p.description, s.colSize AS sizes
-	// 		FROM products p
-	// 		INNER JOIN brands b ON p.brandId = b.id
-	// 		INNER JOIN models m ON p.modelId = m.id AND b.id = m.brandId
-	// 		INNER JOIN sizes s ON p.id = s.id
-	// 		WHERE p.name=${productReq}
-	// 	),
-	// WITH similar_prod AS (
-	// 	SELECT p.id, b.name AS brand, b.id AS brandId, m.id AS modelId, m.name AS modelName, p.name, p.releaseDate, p.colors, p.price, p.description, s.colSize AS sizes
-	// 	FROM products p
-	// 	INNER JOIN brands b ON p.brandId = b.id
-	// 	INNER JOIN models m ON p.modelId = m.id AND b.id = m.brandId
-	// 	INNER JOIN sizes s ON p.id = s.id
-	// 	WHERE p.modelId = (SELECT modelId FROM prod_req) AND p.name <> (SELECT name FROM prod_req)
-	// )
-	// 	SELECT pr.*, ps.* FROM prod_req pr, similar_prod ps;`)
 	await pool.query(SQL`
 		WITH prod_req AS (
 			SELECT p.id, b.name AS brand, b.id AS brandId, m.id AS modelId, m.name AS modelName, p.name, p.releaseDate, p.colors, p.price, p.description, s.colSize AS sizes
@@ -130,28 +78,20 @@ router.post('/product', async (request: Request, response: Response) => {
 			INNER JOIN sizes s ON p.id = s.id
 			WHERE p.modelId = (SELECT modelId FROM prod_req) AND p.name <> (SELECT name FROM prod_req)
 		)
-		SELECT pr.*, ps.* FROM prod_req pr, similar_prod ps;`)
-	// await pool.query(SQL`
-	// 	WITH prod_req AS (
-	// 		SELECT p.id, b.name AS brand, b.id AS brandId, m.id AS modelId, m.name AS modelName, p.name, p.releaseDate, p.colors, p.price, p.description, s.colSize AS sizes
-	// 		FROM products p
-	// 		INNER JOIN brands b ON p.brandId = b.id
-	// 		INNER JOIN models m ON p.modelId = m.id AND b.id = m.brandId
-	// 		INNER JOIN sizes s ON p.id = s.id
-	// 		WHERE p.name=${productReq}
-	// 	),
-	// 	similar_prod AS (
-	// 		SELECT p.id, b.name AS brand, b.id AS brandId, m.id AS modelId, m.name AS modelName, p.name, p.releaseDate, p.colors, p.price, p.description, s.colSize AS sizes
-	// 		FROM products p
-	// 		INNER JOIN brands b ON p.brandId = b.id
-	// 		INNER JOIN models m ON p.modelId = m.id AND b.id = m.brandId
-	// 		INNER JOIN sizes s ON p.id = s.id
-	// 		WHERE p.modelId = (SELECT modelId FROM prod_req) AND p.name <> (SELECT name FROM prod_req)
-	// 	)
-	// 	SELECT productReq AS prreq, similarProducts FROM (SELECT pr.* FROM prod_req pr) AS productReq, (SELECT ps.* FROM similar_prod ps) AS similarProducts;`)
+		SELECT pr.* FROM prod_req pr UNION ALL SELECT ps.* FROM similar_prod ps;`)
 		.then((response: any) => {
-			console.log(response.rows)
-			productQueryResult = response.rows
+			if (response.rows.length !== 0) {
+				rawQueryResult = response.rows
+				productRequested = rawQueryResult.filter((res: any) => res.name === productReq)[0]
+				similarProducts = rawQueryResult.filter((res: any) => res.name !== productReq)
+				productQueryResult = {
+					productReq: productRequested,
+					similarProducts: similarProducts
+				}
+			}
+			else {
+				productQueryResult = { productReq: [], similarProducts: [] }
+			}
 		})
 		.catch((err: any) => {
 			console.log(err)
